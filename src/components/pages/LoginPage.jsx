402299c3feafc2179b736db122925c6ed9332c33
Baba11461 <sallb7677@gmail.com>
2023-11-01 13:49:27 +0100

    ✨ F01: Login>add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 function LoginPage() {
+  //state
+
+  const [inputValue, setInputValue] = useState("");
+
+  //Comportement
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  //Affichage
   return (
     <div>
-      <h1>Login page</h1>
+      <h1>Bienvenue chez nous </h1>
+      <br />
+      <h3>Connectez-vous</h3>
+
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="entre votre prénom..."
+          required
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button>Accédez à votre espace</button>
+      </form>
     </div>
   );
 }
