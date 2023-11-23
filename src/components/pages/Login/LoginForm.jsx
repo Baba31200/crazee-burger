1ad277f9844255fdb5482172120b57cc2a13b9fc
Baba11461 <babaracine1993@gmail.com>
2023-11-23 18:35:34 +0100

    💄 F03:Login>LoginPage add styling

--- src/components/pages/Login/LoginForm.jsx
+++ src/components/pages/Login/LoginForm.jsx
@@ -23,7 +23,7 @@ function LoginForm() {
   return (
     <LoginFormSyled action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <input
         type="text"
@@ -37,7 +37,101 @@ function LoginForm() {
   );
 }
 
-const LoginFormSyled = styled.div`
-  background-color: yellow;
+const LoginFormSyled = styled.form`
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #f56a2e;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
+
+  .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+      /* width: 100%; */
+    }
+    &::placeholder {
+      background: white;
+      color: lightgray;
+    }
+  }
+  .button-with-icon {
+    width: 100%;
+    border: 1px solid red;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    white-space: nowrap;
+    text-decoration: none;
+    line-height: 1;
+
+    padding: 18px 24px;
+    border-radius: 5px;
+    font-size: 15px;
+    font-weight: 800;
+    color: white;
+    background-color: #ff9f1b;
+    border: 1px solid #ff9f1b;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
+    }
+  }
 `;
 export default LoginForm;
