ccd0c8d475dd633dc2abd33bccb456f61dcad09b
Baba11461 <babaracine1993@gmail.com>
2023-12-03 18:50:08 +0100

    🚧 F04:Order>layout>add container

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,16 +1,33 @@
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 function OrderPage() {
   const { username } = useParams();
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <br />
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="container">
+        <h1>Bonjour {username}</h1>
+        <br />
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
+    </OrderPageStyled>
   );
 }
 
 export default OrderPage;
+
+const OrderPageStyled = styled.div`
+  background: orange;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background: yellow;
+    height: 95vh;
+    width: 1400px;
+  }
+`;
