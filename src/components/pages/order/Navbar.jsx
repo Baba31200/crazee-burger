7a06f405f7c0bd6ade107a163c92f7d76b752439
Baba11461 <babaracine1993@gmail.com>
2024-01-01 16:54:21 +0100

    ✨ F04:Order>Navbar>trigger reload onClick

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -3,11 +3,12 @@ import NavbarRightSide from "./NavbarRightSide";
 
 import Logo from "../../reusable-ui/Logo";
 import { theme } from "../../../theme";
+import { refreshPage } from "../../../utils/window";
 
 function Navbar({ username }) {
   return (
     <NavbarStyled>
-      <Logo />
+      <Logo className="logo-order-page" onClick={refreshPage} />
       <NavbarRightSide username={username} />
     </NavbarStyled>
   );
@@ -24,4 +25,8 @@ const NavbarStyled = styled.nav`
   /* align-items: center; */
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
+
+  .logo-order-page {
+    cursor: pointer;
+  }
 `;
