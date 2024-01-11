f0c7afc2b7419f8ccd0016099b5f4c6d36856405
Baba11461 <babaracine1993@gmail.com>
2024-01-11 16:33:34 +0100

    ✨ F06: NAVBAR> add notification toast on  admin button click

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,20 +1,43 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { theme } from "../../../../theme";
+import { useState } from "react";
 
 function NavbarRightSide({ username }) {
+  const [isModeAdmin, setIsmodAdmin] = useState(false);
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    setIsmodAdmin(!isModeAdmin);
+  };
+
   return (
     <NavbarRightSideStyled>
       <ToggleButton
         labelIfUnchecked="ACTIVER LE MODE ADMIN"
         labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+        onToggle={displayToastNotification}
       />
       <Profile username={username} />
+      <ToastContainer />
     </NavbarRightSideStyled>
   );
 }
-
-export default NavbarRightSide;
 
 const NavbarRightSideStyled = styled.div`
   display: flex;
@@ -28,4 +51,24 @@ const NavbarRightSideStyled = styled.div`
   .profile {
     background: yellow;
   }
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
+
+export default NavbarRightSide;
