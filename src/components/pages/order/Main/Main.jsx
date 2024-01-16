c6f2c86efca049fc3db3f22e5cb839d6e54da97c
Baba11461 <babaracine1993@gmail.com>
2024-01-16 20:00:47 +0100

    💄 F07:MAIN>Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,8 +6,10 @@ function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
-
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">Amin</div>
+      </div>
     </MainStyled>
   );
 }
@@ -15,9 +17,10 @@ function Main() {
 export default Main;
 
 const MainStyled = styled.div`
+  border: 3px solid green;
   background: ${theme.colors.background_white};
   /*  */
-  flex: 1;
+  height: calc(95vh - 10vh);
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
@@ -25,8 +28,24 @@ const MainStyled = styled.div`
   display: grid;
 
   grid-template-columns: 1fr;
-  overflow-y: scroll;
+  /* overflow-y: scroll; */
   /* .basket {
     background: pink;
   } */
+
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    .admin {
+      background: red;
+      height: 250px;
+      position: absolute;
+      left: 0px;
+      right: 0px;
+      bottom: 0px;
+    }
+  }
 `;
