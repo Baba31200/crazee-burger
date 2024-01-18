52587ecc4f4ba7b9fb7042eefc8d133204bffe40
Baba11461 <babaracine1993@gmail.com>
2024-01-18 17:55:00 +0100

    ♻️ F07: ADMIN > implement more scalable solution (2e implementation)

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -12,27 +12,13 @@ export default function AdminTabs() {
   const {
     isCollapsed,
     setIsCollapsed,
-    isAddSelected,
-    setIsAddSelected,
-    isEditSelected,
-    setIsEditSelected,
+    currentTabSelected,
+    setCurrentTabSelected,
   } = useContext(OrderContext);
 
-  // comportements
-  const handleClick = () => {
-    setIsCollapsed(!isCollapsed);
-  };
-
-  const handleAddClick = () => {
-    setIsCollapsed(false);
-    setIsAddSelected(true);
-    setIsEditSelected(false);
-  };
-
-  const handleEditClick = () => {
-    setIsCollapsed(false);
-    setIsEditSelected(true);
-    setIsAddSelected(false);
+  const selectTab = (tabSelected) => {
+    setIsCollapsed(false); // tu m'ouvres le pannel
+    setCurrentTabSelected(tabSelected);
   };
 
   return (
@@ -40,27 +26,26 @@ export default function AdminTabs() {
       <Tab
         label=""
         Icon={isCollapsed ? <FiChevronUp /> : <FiChevronDown />}
-        onClick={handleClick}
+        onClick={() => setIsCollapsed(!isCollapsed)}
         className={isCollapsed ? "is-active" : ""}
       />
       <Tab
         label="Ajouter un produit"
         Icon={<AiOutlinePlus />}
-        onClick={handleAddClick}
-        className={isAddSelected ? "is-active" : ""}
+        onClick={() => selectTab("add")}
+        className={currentTabSelected === "add" ? "is-active" : ""}
       />
       <Tab
         label="Modifier un produit"
         Icon={<MdModeEditOutline />}
-        onClick={handleEditClick}
-        className={isEditSelected ? "is-active" : ""}
+        onClick={() => selectTab("edit")}
+        className={currentTabSelected === "edit" ? "is-active" : ""}
       />
     </AdminTabsStyled>
   );
 }
 
 const AdminTabsStyled = styled.div`
-  /* border: 1px solid red; */
   display: flex;
   position: absolute;
   top: -43px;
