61146d5ce8eb3934b2994cfe38ffd22cb42da2b3
Baba11461 <babaracine1993@gmail.com>
2024-01-18 17:35:58 +0100

    ✨ F07: ADMIN > AdminPanel > Add content based on tab selected

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,24 +1,37 @@
-import styled from "styled-components";
-
-import AdminTabs from "./AdminTabs";
-import AdminPanel from "./AdminPanel";
+import React from "react";
 import { useState } from "react";
+import styled from "styled-components";
+import AdminPanel from "./AdminPanel";
+import AdminTabs from "./AdminTabs";
 
-function Admin() {
+export default function Admin() {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isAddSelected, setIsAddSelected] = useState(true);
+  const [isEditSelected, setIsEditSelected] = useState(false);
+
   return (
     <AdminStyled>
-      <AdminTabs isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-      {!isCollapsed && <AdminPanel />}
+      <AdminTabs
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+        isAddSelected={isAddSelected}
+        setIsAddSelected={setIsAddSelected}
+        isEditSelected={isEditSelected}
+        setIsEditSelected={setIsEditSelected}
+      />
+      {!isCollapsed && (
+        <AdminPanel
+          isAddSelected={isAddSelected}
+          isEditSelected={isEditSelected}
+        />
+      )}
     </AdminStyled>
   );
 }
 
-export default Admin;
-
 const AdminStyled = styled.div`
   position: absolute;
-  left: 0px;
-  right: 0px;
-  bottom: 0px;
+  bottom: 0;
+  left: 0;
+  right: 0;
 `;
--- src/components/pages/order/Main/Admin/AdminPanel.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel.jsx
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 
-function AdminPanel() {
-  return <AdminPanelStyled></AdminPanelStyled>;
+export default function AdminPanel({ isAddSelected, isEditSelected }) {
+  return (
+    <AdminPanelStyled>
+      {isAddSelected && <p>Ajouter un produit</p>}
+      {isEditSelected && <p>Modifier un produit</p>}
+    </AdminPanelStyled>
+  );
 }
 
 const AdminPanelStyled = styled.div`
-  border: 1px solid blue;
-
   height: 250px;
-
   background: ${theme.colors.white};
   border: 1px solid ${theme.colors.greyLight};
   box-shadow: ${theme.shadows.subtle};
 `;
-
-export default AdminPanel;
