0a868eda696f7fe81810430743d98f4cab683e39
Baba11461 <babaracine1993@gmail.com>
2024-01-18 17:38:53 +0100

    ✨ F07: ADMIN > Persist PanelAdmin's last state if user leaves and returns to modeAdmin

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,37 +1,47 @@
+import { useContext, useState } from "react";
 import styled from "styled-components";
-import { theme } from "../../../../theme";
-import Menu from "./Menu";
-import Admin from "./Admin/Admin";
-import { useContext } from "react";
 import OrderContext from "../../../../context/OrderContext";
+import { theme } from "../../../../theme";
+import Admin from "./Admin/Admin";
+import Menu from "./Menu";
 
-function Main() {
-  const { isModeAdmin, setIsModeAdmin } = useContext(OrderContext);
+export default function Main() {
+  const { isModeAdmin } = useContext(OrderContext);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isAddSelected, setIsAddSelected] = useState(true);
+  const [isEditSelected, setIsEditSelected] = useState(false);
+
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
       <div className="menu-and-admin">
         <Menu />
-        {isModeAdmin && <Admin />}
+        {isModeAdmin && (
+          <Admin
+            isCollapsed={isCollapsed}
+            setIsCollapsed={setIsCollapsed}
+            isAddSelected={isAddSelected}
+            setIsAddSelected={setIsAddSelected}
+            isEditSelected={isEditSelected}
+            setIsEditSelected={setIsEditSelected}
+          />
+        )}
       </div>
     </MainStyled>
   );
 }
 
-export default Main;
-
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
-  /*  */
   height: calc(95vh - 10vh);
+
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-
   grid-template-columns: 1fr;
-  /* overflow-y: scroll; */
+
   /* .basket {
     background: pink;
   } */
