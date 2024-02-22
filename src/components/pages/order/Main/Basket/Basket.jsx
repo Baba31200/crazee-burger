e3eb83bcb3a5a45fe71ad73c69b837c896f376e1
Baba20242024 <babaracine1993@gmail.com>
2024-02-22 17:49:20 +0100

    ♻️ F10 : BASKET > reusable > Create Header.js

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,33 +1,25 @@
 import styled from "styled-components";
-import { theme } from "../../../../theme";
+import { theme } from "../../../../../theme";
+import Header from "../../../../reusable-ui/Header";
 
 export default function Basket() {
   return (
     <BasketStyled>
-      <div className="head">Head</div>
+      <Header>Head</Header>
       <div className="body">Body</div>
-      <div className="footer">Footer</div>
+      <Header>Footer</Header>
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.div`
   background: pink;
-
   display: flex;
   flex-direction: column;
 
-  .head {
-    background: ${theme.colors.background_dark};
-    height: 70px;
-  }
   .body {
     flex: 1;
     background: ${theme.colors.background_white};
     box-shadow: ${theme.shadows.basket};
-  }
-  .footer {
-    background: ${theme.colors.background_dark};
-    height: 70px;
   }
 `;
---
+++ src/components/reusable-ui/Header.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { theme } from "../../theme";
+
+function Header({ children }) {
+  return <HeaderStyled>{children}</HeaderStyled>;
+}
+
+export default Header;
+
+const HeaderStyled = styled.div`
+  background: ${theme.colors.background_dark};
+  height: 70px;
+`;
