7caff9e59fde31f5117b6956a5979c0269e20ec5
Baba20242024 <babaracine1993@gmail.com>
2024-02-24 12:38:57 +0100

    ♻️ F10 : BASKET > Add Footer

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
-import Header from "../../../../reusable-ui/Header";
+// import Header from "../../../../reusable-ui/Header";
 import Totale from "./Totale";
 import { formatPrice } from "../../../../../utils/maths";
+import Footer from "./Footer";
 
 export default function Basket() {
   return (
     <BasketStyled>
       <Totale amountTopay={formatPrice(0)} />
       <div className="body">Body</div>
-      <Header>Footer</Header>
+      {/* <Header>Footer</Header> */}
+      <Footer />
     </BasketStyled>
   );
 }
