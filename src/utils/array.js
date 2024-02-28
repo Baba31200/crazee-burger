f9be2687c490939833af506f8b14a3d56b59cdb7
Baba20242024 <babaracine1993@gmail.com>
2024-02-28 12:53:18 +0100

    ♻️ F11: BASKET > Add Product > Create incrementProductAlreadyInBasket

--- src/utils/array.js
+++ src/utils/array.js
@@ -5,3 +5,19 @@ export const deepClone = (array) => {
 export const find = (id, array) => {
   return array.find((itemInArray) => itemInArray.id === id);
 };
+
+export const findIndex = (idWithUnknowwIndex, array) => {
+  return array.findIndex(
+    (itemInArray) => itemInArray.id === idWithUnknowwIndex
+  );
+};
+// const fruits = [{ nom: "Abricot" }, { nom: "Banane" }]
+// const fruitsShallowCopy = [...fruits]
+// const fruitsDeepCopy = deepClone(fruits)
+
+// //fruitsShallowCopy[1].nom = "Cerise"
+// fruitsDeepCopy[1].nom = "Cerise"
+
+// console.log("fruits: ", fruits)
+// //console.log("fruitsShallowCopy: ", fruitsShallowCopy)
+// console.log("fruitsDeepCopy: ", fruitsDeepCopy)
