a146c7cbecba56efe79360700348bd69fa0e7530
Baba20242024 <babaracine1993@gmail.com>
2024-03-05 19:40:01 +0100

    🚧 F13: MENU > Firestore > Add resetMenu()

--- src/hooks/useMenu.js
+++ src/hooks/useMenu.js
@@ -48,8 +48,9 @@ export const useMenu = () => {
     setMenu(menuCopy);
   };
 
-  const resetMenu = () => {
-    setMenu(fakeMenu.MEDIUM);
+  const resetMenu = (username) => {
+    setMenu(fakeMenu.SMALL);
+    syncBothMenus(username, fakeMenu.SMALL);
   };
 
   return { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu };
