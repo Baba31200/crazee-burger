52658d91841841116c5dc9c5d3fd5314678f35e4
Baba20242024 <babaracine1993@gmail.com>
2024-03-20 14:50:02 +0100

    ♻️ F14: ADMIN > Panel > add animation onAppear

--- src/components/pages/order/Main/MainRightSide/MainRightSide.jsx
+++ src/components/pages/order/Main/MainRightSide/MainRightSide.jsx
@@ -5,13 +5,22 @@ import OrderContext from "../../../../../context/OrderContext";
 import { theme } from "../../../../../theme";
 import Admin from "./Admin/Admin";
 import Menu from "./Menu/Menu";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { adminAnimation } from "../../../../../theme/animations";
+
 export default function MainRightSide() {
   const { isModeAdmin } = useContext(OrderContext);
 
   return (
     <MainRightSideStyled>
       <Menu />
-      {isModeAdmin && <Admin />}
+      {isModeAdmin && (
+        <TransitionGroup className="transition-group">
+          <CSSTransition appear classNames="admin" timeout={500}>
+            <Admin />
+          </CSSTransition>
+        </TransitionGroup>
+      )}
     </MainRightSideStyled>
   );
 }
@@ -21,4 +30,6 @@ const MainRightSideStyled = styled.div`
   overflow-y: hidden;
   display: grid;
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  ${adminAnimation}
 `;
